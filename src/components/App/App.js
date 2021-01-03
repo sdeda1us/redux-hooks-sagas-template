401ce545fe1171c869086ce023d67e7d92331417
sdeda1us <steven.amloney@gmail.com>
2021-01-02 22:57:41 -0600

    added styled components to the template

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import './App.css';
+import styled, {css} from 'styled-components';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Hello from template</h1>
-        </header>
-        <br/>
-      </div>
-    );
-  }
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Hello from template</h1>
+      </header>
+      <br/>
+    </div>
+  );
 }
 
-export default App;
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import {takeEvery, put, call} from 'redux-saga/effects';
+import Axios from 'axios';
 
-const storeInstance = createStore(
-    // This is a reducer... we'll talk about it in a minute, hang on for a second
-    () => {
-        console.log(`Hey!!! I'm a reducer y'all!!!`);
-    },
-);
+
+const sagaMiddleware = createSagaMiddleware();
+
+//----------------------WATCHER SAGA------------------------------//
+function* rootSaga() {
+
+}
+
+const templateReducer = (state=[], action) => {
+    return state;
+}
+
+const storeInstance = createStore( 
+    (combineReducers({
+        templateReducer
+    })),
+        applyMiddleware(sagaMiddleware, logger)
+    );
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
